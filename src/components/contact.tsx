75724c7b35006350821e8950a727473f45c5aeb5
Olimov Abdulloh <lochin.tj@gmail.com>
2024-04-02 00:06:03 +0500

    send message to telegram

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,6 +1,47 @@
+import { useState } from 'react';
 import '../App.css';
+import { toast } from 'react-toastify';
 
 const Contact = () => {
+  const [message, setMessage] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  const sendMessage = async () => {
+    const token = '7014941655:AAGccnh01y1JkW9E3ggcwSlg5NwoaEKQdL8';
+    const chatId = '5923880668'; // The chat ID of the user or group you want to send the message to
+    const url = `https://api.telegram.org/bot${token}/sendMessage`;
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          chat_id: chatId,
+
+          text: `Name: ${name}\nPhone Number: ${phoneNumber}\nMessage: ${message}`,
+        }),
+      });
+
+      if (response.ok) {
+        console.log('Message sent successfully');
+
+        toast.success('Успешно отправленно');
+        setMessage('');
+        setPhoneNumber('');
+        setName('');
+      } else {
+        console.error('Failed to send message');
+        toast.error(await response.text());
+        throw new Error('Server error');
+      }
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  };
+
   return (
     <div id="connect" className="bg-[#202733] py-24">
       <div className="containers max-sm:px-2">
@@ -24,19 +65,28 @@ const Contact = () => {
               type="text"
               className="border-input ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full max-w-[375px] rounded-full border bg-[#d9d9d9] px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
               placeholder="Ismingiz"
-              value=""
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
             <input
               type="number"
               className="border-input ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring no-spinner flex h-10 w-full max-w-[375px] rounded-full border bg-[#d9d9d9] px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
               placeholder="Telefon raqamingiz"
-              value=""
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
             />
             <textarea
               className=" border-input  ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex min-h-[140px] w-full max-w-[375px] resize-none rounded-2xl border bg-[#d9d9d9] px-3 py-2 text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
               placeholder="Sizning Xabaringiz..."
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
             ></textarea>
-            <button className="ring-offset-background focus-visible:ring-ring  text-primary-foreground scale inline-flex h-10 w-36 cursor-pointer items-center justify-center whitespace-nowrap rounded-md rounded-l-full rounded-r-full border-2 bg-transparent bg-white px-10 py-2 text-sm font-medium transition-colors hover:border-gray-500 hover:bg-transparent hover:text-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
+            <button
+              onClick={(e) => {
+                sendMessage(), e.preventDefault();
+              }}
+              className="ring-offset-background focus-visible:ring-ring  text-primary-foreground scale inline-flex h-10 w-36 cursor-pointer items-center justify-center whitespace-nowrap rounded-md rounded-l-full rounded-r-full border-2 bg-transparent bg-white px-10 py-2 text-sm font-medium transition-colors hover:border-gray-500 hover:bg-transparent hover:text-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+            >
               Jo'natish
             </button>
           </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 // Order seems to matter. If Mantine is imported after tailwind, the tailwind class passed with className is not applied.
-import '@mantine/core/styles.css';
-import './index.css';
-import './i18.jsx';
 import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './i18.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <MantineProvider>
+    <ToastContainer />
     <App />
   </MantineProvider>,
 );
