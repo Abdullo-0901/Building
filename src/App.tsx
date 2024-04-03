ea96e5d20bb12120e9a9be86c70c8e6dbc2089a3
Olimov Abdulloh <lochin.tj@gmail.com>
2024-04-03 16:59:19 +0500

    remover partners file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import Layout from './pages/layout';
+import Headers from './components/Headers/headers';
+import Company from './components/company';
+import Contact from './components/contact';
+import Home from './components/home/home';
+import { Information } from './components/information';
+import News from './components/news';
+import Project from './components/project';
+import Services from './components/services';
 
 const App = () => {
-  return <Layout />;
+  return (
+    <div className="overflow-hidden">
+      <Headers />
+      <Home />
+      <section id="services" className="bg-[#3d5a6a]">
+        <Services />
+      </section>
+      <Company />
+      <Information />
+      <Project />
+      <News />
+      <Contact />
+      <footer />
+    </div>
+  );
 };
 
 export default App;
